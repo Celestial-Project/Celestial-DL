3b5fafa69eef7040cb869eac8d37ff139404bc14
Jirayu Kaewsing <strixz.self@gmail.com>
2023-01-28 12:26:28 +0700

    Init commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import json
+import time
+import pickle
+import pythainlp
+import numpy as np
+
+from tensorflow import keras
+
+MAX_LENGTH = 20
+
+with open('./intents.json', encoding = 'utf-8') as f:
+    data1 = json.load(f)
+
+with open('./intents2.json', encoding = 'utf-8') as f:
+    data2 = json.load(f)
+
+with open('label_encoder.pickle', 'rb') as enc:
+    label_encoder = pickle.load(enc)
+
+with open('word_label_encoder.pickle', 'rb') as enc:
+    word_encoder = pickle.load(enc)
+
+data = data1['intents_th'] + data2['intents_en']
+model = keras.models.load_model('chat_model')
+
+def to_sequences(message) -> list[int]:
+    msg = pythainlp.word_tokenize(message, engine = 'nercut', keep_whitespace = False)
+    return word_encoder.transform(msg)
+
+while True:
+
+    input_message = input('usr> ').lower()
+
+    start = time.perf_counter()
+
+    if input_message == 'quit':
+        break
+    
+    try:
+
+        sequence = to_sequences(input_message)
+        print(sequence)
+        
+        result = model.predict(keras.preprocessing.sequence.pad_sequences([sequence], truncating = 'post', maxlen = MAX_LENGTH))
+        print(np.argmax(result))
+
+        tag = label_encoder.inverse_transform([np.argmax(result)])
+
+        for i in data:
+            if i['tag'] == tag:
+                print(f'celestial: {np.random.choice(i["responses"])}')
+
+    except ValueError as e:
+        print('Unkown responses')
+        print(e)
+
+    end = time.perf_counter()
+
+    elasped = start - end
+    print(f'Time elasped: {round((end - start) * 1000, 4)} ms')
---
+++ train.py
@@ -0,0 +1,68 @@
+import json 
+import pickle
+import pythainlp
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+
+with open('./intents.json', encoding = 'utf-8') as f:
+    data1 = json.load(f)
+
+with open('./intents2.json', encoding = 'utf-8') as f:
+    data2 = json.load(f)
+
+data = data1['intents_th'] + data2['intents_en']
+
+training_sentences = []
+training_labels = []
+labels = []
+responses = []
+
+for intent in data:
+    
+    for pattern in intent['patterns']:
+        training_sentences.append(pattern.lower())
+        training_labels.append(intent['tag'])
+    
+    responses.append(intent['responses'])
+    
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+        
+num_classes = len(labels)
+
+wordlist = [pythainlp.word_tokenize(seq, engine = 'nercut', keep_whitespace = False) for seq in training_sentences]
+wordset = list(set([j for i in wordlist for j in i]))
+
+label_encoder = LabelEncoder()
+label_encoder.fit(training_labels)
+
+training_labels = label_encoder.transform(training_labels)
+
+word_label_encoder = LabelEncoder()
+word_label_encoder.fit(wordset)
+
+encoded_sentences = pad_sequences([word_label_encoder.transform(wl) for wl in wordlist], truncating = 'post', maxlen = 20)
+
+max_length = 20
+model = Sequential()
+
+model.add(Embedding(len(wordset), 16, input_length = 20))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(16, activation = 'relu'))
+model.add(Dense(16, activation = 'relu'))
+model.add(Dense(16, activation = 'relu'))
+model.add(Dense(num_classes, activation = 'softmax'))
+
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+model.fit(encoded_sentences, np.array(training_labels), epochs = 800)
+
+model.save("./model/chat_model")
+
+with open('./model/label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(label_encoder, ecn_file, protocol = pickle.HIGHEST_PROTOCOL)
+
+with open('./model/word_label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(word_label_encoder, ecn_file, protocol = pickle.HIGHEST_PROTOCOL)
