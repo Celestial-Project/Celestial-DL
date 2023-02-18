731020895676ba15d789491657dcf87ce61f5ef2
Jirayu Kaewsing <strixz.self@gmail.com>
2023-02-18 21:02:15 +0700

    Change RPC message

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ client = commands.Bot(
     command_prefix = '::</!' if use_debug_mode else '</!', 
     intents = intents, 
     help_command = None,
-    activity = discord.Game(name = '<!help> for more info.')
+    activity = discord.Game(name = '</!help> for more info.')
 )   
 
 @client.event
