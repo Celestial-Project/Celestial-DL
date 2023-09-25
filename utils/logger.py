b46a2988789a0bd1500c61e20aa07f7dfc7af6f1
Jirayu Kaewsing <strixz.self@gmail.com>
2023-09-25 18:59:31 +0700

    Isolate terminal clear into function

--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,5 @@
+import os
+
 def info_log(log_message: str) -> None:
     print(f'\u001b[45;1m ** \u001b[0m {log_message}')
 
@@ -12,3 +14,6 @@ def incoming_log(log_message: str) -> None:
 
 def outgoing_log(log_message: str) -> None:
     print(f'\u001b[41;1m <- \u001b[0m {log_message}')
+
+def clear_log() -> None:
+    os.system('cls' if os.name == 'nt' else 'clear')
