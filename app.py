595a2177a0acf9c00d58a763c3fa8da43c0083bc
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2024-03-17 21:48:46 +0700

    change process_message() args to be up to date

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS, cross_origin
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from utils.loader import load_chat_model
 from utils.logger import info_log, clear_log
 from utils.chat_processing import process_message
 
@@ -16,9 +17,11 @@ flags_parser = argparse.ArgumentParser(
 
 flags_parser.add_argument('-p', '--port', nargs = '?', default = 21250, type = int)
 flags_parser.add_argument('-d', '--debug', action = 'store_true')
+flags_parser.add_argument('-m', '--model', type=str)
 
 debug = flags_parser.parse_args().debug
 PORT = flags_parser.parse_args().port
+selected_model = flags_parser.parse_args().model
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -26,11 +29,14 @@ limiter = Limiter(get_remote_address, app = app)
 
 app.config['MAX_CONTENT_LENGTH'] = 1024
 
+(model, model_name, data, label_encoder, word_encoder) = load_chat_model() if not selected_model else load_chat_model(selected_model)
+
 def show_ready(port: int, debug: bool) -> None:
 
     clear_log()
     
     info_log('Chat REST API ready!')
+    info_log(f'Model name: {model_name}')
     info_log(f'Mode: {"Debug" if debug else "Production"}')
     info_log('Press ctrl+c to exit.')
     info_log(f'API running on: http://localhost:{port}/chat-dl')
@@ -46,12 +52,12 @@ def send_response():
     if body is None or body['message'] == '':
         return ({}, 400)
     
-    return ({'chat': process_message(body['message'])}, 200)
+    return ({'chat': process_message(body['message'], model, data, label_encoder, word_encoder, debug = debug)}, 200)
 
 
 def main():
 
-    process_message('hello')
+    process_message('hello', model, data, label_encoder, word_encoder)
 
     if debug:
         
